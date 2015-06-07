a93b52849466c60839a220d9a8b936b878577e81
George Wilson <georgejameswilson@googlemail.com>
2015-06-07 14:40:19 +0100

    Missing semicolon

--- jstats.php
+++ jstats.php
@@ -4,7 +4,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-jimport('joomla.filesystem.file')
+jimport('joomla.filesystem.file');
 
 /**
  * Class plgSystemJstats
